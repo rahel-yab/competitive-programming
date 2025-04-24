856007e4686f40b4b7536ce5778e0807cd0bdf98
rahel yabebal <135514994+rahel-yab@users.noreply.github.com>
2025-04-24 22:36:50 +0300

    Created 'B - The Lord of Medianfell' by A2SV Hub

---
+++ A2SV G6 - Round #8 24-Apr-2025/B - The Lord of Medianfell 341848.py
@@ -0,0 +1,48 @@
+# Problem: B - The Lord of Medianfell - https://codeforces.com/gym/599383/problem/B
+
+import math
+from math import gcd
+import sys, threading
+from bisect import bisect_left, bisect_right
+from heapq import heapify, heappop, heappush
+from collections import defaultdict, Counter, deque
+input = lambda: sys.stdin.readline().rstrip()
+
+def si():
+    return input()
+def ii():
+    return int(input())
+def lsi():
+    return input().split()
+def mi():
+    return map(int, input().split())
+def li():
+    return list(map(int, input().split()))
+# =========================================
+
+"""
+# Note
+
+"""
+
+def solve(): 
+   n, s = [int(i) for i in input().split()]
+   m = n // 2 + 1 
+   print(s // m) 
+   
+
+
+# =========================================
+def main():
+    t = ii()
+    for _ in range(t):
+        solve()
+
+# =========================================
+main()
+# if __name__ == '__main__':
+#   sys.setrecursionlimit(1 << 30)
+#   threading.stack_size(1 << 27)
+#   main_thread = threading.Thre ad(target=main)
+#   main_thread.start()
+#   main_thread.join()
