41c35fae4008292caa8b5c1ed92218364b65ace0
rahel yabebal <135514994+rahel-yab@users.noreply.github.com>
2025-09-08 22:09:16 +0300

    Created 'Team Composition: Programmers and Mathematicians' by A2SV Hub

---
+++ 08-Sep-2025/Team Composition: Programmers and Mathematicians 392003.py
@@ -0,0 +1,43 @@
+# Problem: Team Composition: Programmers and Mathematicians - https://codeforces.com/problemset/problem/1611/B
+
+import math
+import heapq
+import sys, threading
+from bisect import bisect_left, bisect_right
+from collections import defaultdict, Counter, deque
+
+input = lambda: sys.stdin.readline().rstrip()
+def si():
+    return input()
+def ii():
+    return int(input())
+def lsi():
+    return input().split()
+def mi():
+    return map(int, input().split())
+def li():
+    return list(map(int, input().split()))
+yn = lambda condition: 'YES' if condition else 'NO'
+
+# ========================================
+# =========================================
+def solve():
+    a , b = mi()
+    val = (a+b)//4
+    print(min(val,a,b))
+
+
+    return
+# =========================================
+def main():
+    t = ii()
+    for _ in range(t):
+        solve()
+main()
+
+# if __name__ == '__main__':
+#     sys.setrecursionlimit(1 << 25)
+#     threading.stack_size(1 << 27)
+#     thread = threading.Thread(target=main)
+#     thread.start()
+#     thread.join()
