6063dc359963f4c077515381f331c5102e83d417
rahel yabebal <135514994+rahel-yab@users.noreply.github.com>
2025-09-10 20:56:17 +0300

    Created 'Separate Black and white balls' by A2SV Hub

---
+++ 10-Sep-2025/Separate Black and white balls 392470.py
@@ -0,0 +1,16 @@
+# Problem: Separate Black and white balls - https://leetcode.com/problems/separate-black-and-white-balls/
+
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+        swap = 0
+        black = 0
+        
+        for c in s:
+            if c == "0":
+                swap += black
+            else:
+                black += 1
+
+        return swap
+
+        
