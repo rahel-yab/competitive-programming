c0efafa0ad21c3a91047cdae92ef7e43991d921c
rahel yabebal <135514994+rahel-yab@users.noreply.github.com>
2025-09-15 22:20:25 +0300

    Created 'Three Divisors' by A2SV Hub

---
+++ 15-Sep-2025/Three Divisors 393352.py
@@ -0,0 +1,13 @@
+# Problem: Three Divisors - https://leetcode.com/problems/three-divisors/description/?envType=problem-list-v2&envId=number-theory
+
+class Solution:
+    def isThree(self, n: int) -> bool:
+        c = 0
+        for i in range(1,n+1):
+            if n % i == 0:
+                c += 1
+
+        if c == 3:
+            return True
+        
+        return False
