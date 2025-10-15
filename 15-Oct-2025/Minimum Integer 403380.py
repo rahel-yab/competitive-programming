d3514f3329c14ba463635d3138bc76e4c284dc1d
Rahel Yabebal <135514994+rahel-yab@users.noreply.github.com>
2025-10-15 17:48:42 +0300

    Created 'Minimum Integer' by A2SV Hub

---
+++ 15-Oct-2025/Minimum Integer 403380.py
@@ -0,0 +1,46 @@
+# Problem: Minimum Integer - https://codeforces.com/problemset/problem/1101/A
+
+import math
+import heapq
+import sys, threading
+from bisect import bisect_left, bisect_right
+from collections import defaultdict, Counter, deque
+
+input = lambda: sys.stdin.readline().rstrip()
+def si():
+    return input()
+def ii():
+    return int(input())
+def lsi():
+    return input().split()
+def mi():
+    return map(int, input().split())
+def li():
+    return list(map(int, input().split()))
+yn = lambda condition: 'YES' if condition else 'NO'
+
+# ========================================
+# =========================================
+def solve():
+    l, r , d = mi()
+    if d < l:
+        print(d)
+        return
+    if d > r:
+        print(d)
+        return
+    val =r//d + 1
+    print(val*d)
+# =========================================
+def main():
+    t = ii()
+    for _ in range(t):
+        solve()
+main()
+
+# if __name__ == '__main__':
+#     sys.setrecursionlimit(1 << 25)
+#     threading.stack_size(1 << 27)
+#     thread = threading.Thread(target=main)
+#     thread.start()
+#     thread.join()
