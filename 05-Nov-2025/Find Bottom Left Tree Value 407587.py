9d13118bb65ab904741b4128c5bc4587ec5636bf
Rahel Yabebal <135514994+rahel-yab@users.noreply.github.com>
2025-11-05 23:24:50 +0300

    Created 'Find Bottom Left Tree Value' by A2SV Hub

---
+++ 05-Nov-2025/Find Bottom Left Tree Value 407587.py
@@ -0,0 +1,29 @@
+# Problem: Find Bottom Left Tree Value - https://leetcode.com/problems/find-bottom-left-tree-value/description/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return root
+        
+        ans = None
+        q = deque([root])
+        while q:
+            curr = []
+            for _ in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                curr.append(node)
+            
+            ans = curr
+        
+        return ans[0].val
+
